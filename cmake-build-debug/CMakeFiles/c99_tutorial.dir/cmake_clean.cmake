cd50a2d4cda9f5d5b39b541ea5b029fc5d8def58
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-10-16 00:16:15 +0530

    c99

---
+++ cmake-build-debug/CMakeFiles/c99_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c99_tutorial.dir/main.c.obj"
+  "c99_tutorial.exe"
+  "c99_tutorial.exe.manifest"
+  "c99_tutorial.lib"
+  "c99_tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c99_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
